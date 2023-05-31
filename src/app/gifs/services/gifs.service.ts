9e67a3002e4eca760b7ceeb236c1371df952cbe0
Hector Martinez <hmartinezmoreira81@gmail.com>
2023-05-30 23:02:29 -0600

    feat: http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchResource, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagsHistory: string[] = [];
-  constructor() {}
+  public gifList: Gif[] = [];
+  constructor(private http: HttpClient) {}
+
+  private apiKey: string = '';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -19,8 +25,17 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
-  public searchTag(tag: string) {
+  public async searchTag(tag: string): Promise<void> {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http
+      .get<SearchResource>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => (this.gifList = resp.data));
   }
 }
